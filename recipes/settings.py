31cc49a55ca908087866ac144b681fa48e757f88
hupratt <hprattdo@gmail.com>
2023-03-13 22:37:20 +0100

    set up smtp

--- recipes/settings.py
+++ recipes/settings.py
@@ -400,10 +400,13 @@ CORS_ALLOW_METHODS = ['GET', 'OPTIONS', 'POST']
 # future versions of django will make undeclared default django.db.models.BigAutoField which will force migrations on all models
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
 EMAIL_HOST = os.getenv('EMAIL_HOST', '')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', 25))
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', '')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', '')
+
 EMAIL_USE_TLS = bool(int(os.getenv('EMAIL_USE_TLS', False)))
 EMAIL_USE_SSL = bool(int(os.getenv('EMAIL_USE_SSL', False)))
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL', 'webmaster@localhost')
